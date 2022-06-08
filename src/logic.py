761861a056d1d28320d9b435c015ae1920bff047
BAN03 <angeldejesusatanasiodiaz@gmail.com>
2022-06-08 01:00:25 -0500

    add function to clean

--- src/logic.py
+++ src/logic.py
@@ -7,15 +7,18 @@ class Animal():
         global df
         df = pd.read_csv("src/data.csv")
         self.max_stat = 100
-        self.stats = [df["hungry"][0],df["thirst"][0],df["happiness"][0]]
+        self.stats = [df["hungry"][0],df["thirst"][0],df["happiness"][0],df["clean"][0]]
+
 
     def write_csv(self):
         df.to_csv("src/data.csv", index=False)
 
-    def edit_csv(self, hungry=100, thirst=100, happiness=100):
+
+    def edit_csv(self, hungry=100, thirst=100, happiness=100, clean=100):
         df["hungry"][0] = hungry
         df["thirst"][0] = thirst
         df["happiness"][0] = happiness
+        df["clean"][0] = clean
         self.write_csv()
 
 
@@ -26,18 +29,6 @@ class Animal():
             df["hungry"][0] = self.max_stat
             self.write_csv()
         
-        
-
-    def heal(self):
-        while True:
-            if not df["life"][0] >= self.max_stat:
-                if sum(self.stats) >= 250:
-                    df["life"][0] += 3
-                    sleep(5)
-            else:
-                df["life"][0] = self.max_stat
-                self.write_csv()
-                return False
 
     def drink(self):
         if not df["thirst"][0] >= self.max_stat:
@@ -45,4 +36,23 @@ class Animal():
         else:
             df["thirst"][0] = self.max_stat
             self.write_csv()
+        
 
+    def clean(self):
+        if not df["clean"][0] >= self.max_stat:
+            df["clean"][0] += 25
+        else:
+            df["clean"][0] = self.max_stat
+            self.write_csv()
+
+
+    def heal(self):
+        while True:
+            if not df["life"][0] >= self.max_stat:
+                if sum(self.stats) >= (self.stats - 50):
+                    df["life"][0] += 3
+                    sleep(5)
+            else:
+                df["life"][0] = self.max_stat
+                self.write_csv()
+                return False
